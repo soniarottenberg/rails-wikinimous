f82d5a5c0593171636973903287db0bc9aac27d3
Sonia Rottenberg <sonia.rottenberg@gmail.com>
2019-05-18 19:15:50 +0200

    index ok

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,16 @@
+class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+  def index
+    @articles = Article.all
+  end
+
+  def show
+
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+articleOne = Article.create([{title: Faker::Book.title}, {content: Faker::Lorem.sentence}])
+articleTwo = Article.create([{title: Faker::Book.title}, {content: Faker::Lorem.sentence}])
+articleThree = Article.create([{title: Faker::Book.title}, {content: Faker::Lorem.sentence}])
+articleFour = Article.create([{title: Faker::Book.title}, {content: Faker::Lorem.sentence}])
+articleFive = Article.create([{title: Faker::Book.title}, {content: Faker::Lorem.sentence}])
+articleSix = Article.create([{title: Faker::Book.title}, {content: Faker::Lorem.sentence}])
+articleSeven = Article.create([{title: Faker::Book.title}, {content: Faker::Lorem.sentence}])
+articleEight = Article.create([{title: Faker::Book.title}, {content: Faker::Lorem.sentence}])
+articleNine = Article.create([{title: Faker::Book.title}, {content: Faker::Lorem.sentence}])
+articleTen = Article.create([{title: Faker::Book.title}, {content: Faker::Lorem.sentence}])
